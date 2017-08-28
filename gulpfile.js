a358ce39dba2c4fd4ade720889bec78e0a594232
Wence Criado <wence74@gmail.com>
2017-08-28 19:48:47 +0200

    Creo el archivo de tareas GULP

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+// Importarmos la libería de gulp
+
+var gulp = require("gulp");
+
+
+// Definimos la tarea por defecto
+
+gulp.task("default",function(){
+console.log("Gulp Andando !");
+});
+
+// Definimos la tarea Compilar saas
+
+gulp.task("sass", function (){
+    console.log("Compilando SASS!!");
+});
